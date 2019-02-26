64d1c3e9ada9647333547b73ea8c181040644197
claire <github email address>
2019-02-26 14:53:46 -0600

    separated Card Component, switched from react-native-base to vanilla js, added buttons

--- components/Match.js
+++ components/Match.js
@@ -1,90 +1,60 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
 import { connect } from 'react-redux'
-
-const defaultImg = {uri: 'https://i.pinimg.com/originals/9f/81/2d/9f812d4cf313e887ef99d8722229eee1.jpg'}
-
-// const cards = [
-//   {
-//     text: 'Card One',
-//     name: 'One',
-//     image: defaultImg
-//   },
-//   {
-//     text: 'Card TWO',
-//     name: 'TWO',
-//     image: defaultImg
-//   },
-//   {
-//     text: 'Card Three',
-//     name: 'Three',
-//     image: defaultImg
-//   },
-
-  
-// ];
+import { StyleSheet, View, Image, Text, PanResponder, Animated, Dimensions } from 'react-native';
+import _Card from './Card'
 
 
 class _Match extends Component {
+  state= {
+    profileIndex: 0
+  }
+
+  nextCard = () => {
+    this.setState({profileIndex: this.state.profileIndex + 1})
+  }
+
+  swipedRight = () => {
+    console.log('YOU SWIPED RIGHT')
+  }
+
+  swipedLeft = () => {
+    console.log('YOU SWIPED LEFT')
+  }
+
+  pressedRight = () => {
+    console.log('YOU PRESSED RIGHT')
+  }
+
+  pressedLeft = () => {
+    console.log('YOU PRESSED LEFT')
+  }
 
 
   render() {
-    
-    const cards = this.props.potentials
-    
+   
     return (
-      <Container>
-        {/* <Header /> */}
-        <Text onPress={this.props.getMatchees} style={{fontSize: 25}}>
+      <View style={{flex:1}}>
+        {this.props.potentials.map((profile) => {
+          return (
+            <_Card
+              key={profile.id}
+              profile={profile}
+              onSwipeOff={this.nextCard}
+              onSwipeRight={this.swipedRight}
+              onSwipeLeft={this.swipedLeft}
+              onPressLeft={this.pressedLeft}
+              onPressRight={this.pressedRight}
+            />
+          )
+        })}
+         
 
-            GET MATCHEES
-
-        </Text> 
-       
-        <View>
-          <DeckSwiper
-            dataSource={cards}
-            renderItem={item =>
-              <Card 
-                style={{ elevation: 3 }}
-                onSwipeRight={() => console.log('YOU SWIPED RIGHT ON', item)}
-                onSwipeLeft={() => console.log('YOU SWIPED LEFT ON', item)}
-              >
-                <CardItem>
-                  <Left>
-                    <Thumbnail source={defaultImg} />
-                    <Body>
-                      <Text>{item.name}</Text>
-                      <Text note>{item.email}</Text>
-                    </Body>
-                  </Left>
-                </CardItem>
-
-
-                <CardItem cardBody>
-                  <Image style={{ height: 300, flex: 1 }} source={defaultImg} />
-                </CardItem>
-
-
-                
-                <CardItem>
-                  <Icon name="heart" style={{ color: '#ED4A6A' }} />
-                  <Text>{item.bio}</Text>
-                </CardItem>
-              </Card>
-            }
-          />
-
-       
-        </View> 
-
-      </Container>
-    );
+      </View>
+    )
   }
+
+
 }
-
-
 
 
 
@@ -104,3 +74,148 @@ const mapDispatchToProps = (dispatch) => ({
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(_Match)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import { Image } from 'react-native';
+// import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
+// import { connect } from 'react-redux'
+
+// const cards = [
+  //   {
+  //     text: 'Card One',
+  //     name: 'One',
+  //     image: defaultImg
+  //   },
+  //   {
+  //     text: 'Card TWO',
+  //     name: 'TWO',
+  //     image: defaultImg
+  //   },
+  //   {
+  //     text: 'Card Three',
+  //     name: 'Three',
+  //     image: defaultImg
+  //   },
+  
+    
+  // ];
+  
+  
+  // class _Match extends Component {
+  
+  //   state={
+  //     current_card: {}
+  //   }
+  
+  
+  //   swipedRight = () => {
+  //     console.log('YOU SWIPED RIGHT ON')
+  //   }
+  
+  //   renderItemsForCards = (item) => {
+  
+  //    console.log('ITEM', item)
+  
+  //     return (
+  //       <Card style={{ elevation: 3 }}>
+  //         <CardItem>
+  //           <Left>
+  //             <Thumbnail source={defaultImg} />
+  //             <Body>
+  //               <Text>{item.name}</Text>
+  //               <Text note>{item.email}</Text>
+  //             </Body>
+  //           </Left>
+  //         </CardItem>
+  //         <CardItem cardBody>
+  //           <Image style={{ height: 300, flex: 1 }} source={defaultImg} />
+  //         </CardItem>
+  //         <CardItem>
+  //           <Icon name="heart" style={{ color: '#ED4A6A' }} />
+  //           <Text>{item.bio}</Text>
+  //         </CardItem>
+  //       </Card>
+    
+  //     )
+  //   }
+  
+  //   render() {
+      
+  //     const cards = this.props.potentials
+      
+  //     // console.log("STATE in MATCH", this.state)
+      
+  //     return (
+  //       <Container>
+  //         {/* <Header /> */}
+  //         <Text onPress={this.props.getMatchees} style={{fontSize: 25}}>
+  
+  //             GET MATCHEES
+  
+  //         </Text> 
+         
+  //         <View>
+  //           <DeckSwiper
+  //               // onSwipeRight={() => this.swipedRight()}
+  //               // onSwipeLeft={this.swipedLeft}
+  //               dataSource={cards}
+  //               renderItem={(item) => this.renderItemsForCards(item)}
+                  
+  //               //   <Card 
+  //               //     style={{ elevation: 3 }}
+  //               //   >
+  
+  //               //   {/* <>{current_card=item}</>  */}
+  
+  
+      
+  //               //   <CardItem>
+  //               //     <Left>
+  //               //       <Thumbnail source={defaultImg} />
+  //               //       <Body>
+  //               //         <Text>{item.name}</Text>
+  //               //         <Text note>{item.email}</Text>
+  //               //       </Body>
+  //               //     </Left>
+  //               //   </CardItem>
+  
+  
+  //               //   <CardItem cardBody>
+  //               //     <Image style={{ height: 300, flex: 1 }} source={defaultImg} />
+  //               //   </CardItem>
+  
+  
+                  
+  //               //   <CardItem>
+  //               //     <Icon name="heart" style={{ color: '#ED4A6A' }} />
+  //               //     <Text>{item.bio}</Text>
+  //               //   </CardItem>
+  //               // </Card>
+                
+              
+  //           />
+  
+         
+  //         </View> 
+  
+  //       </Container>
+  //     );
+  //   }
+  // }
+  
+  
+  
