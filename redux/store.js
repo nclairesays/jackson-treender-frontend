9903ace7fe9e2067480e32a649de7e3f7009da71
claire <github email address>
2019-02-27 09:48:46 -0600

    changed async fetch - does not work

--- redux/store.js
+++ redux/store.js
@@ -44,22 +44,29 @@ const rootReducer = (state, action) => {
                 ...state, user: action.user
             };
         case 'CREATE_USER': 
-            server.post(`${API_URL}/users`,{
-                name: action.name,
-                email: action.email,
-                password: action.password
+            fetch(`${API_URL}/users`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: action.name,
+                    email: action.email,
+                    password: action.password
+                })
             })
-            .then(() => {
+            .then(resp => resp.json())
+            .then(res => {
                 // this.props.history.push(`/login`)  
-                console.log(this.props)
+                console.log(res)
             })
 
             .catch(error => {
                  console.log('ERRORS GOT IN THE WAY: ', error)
             })
         case 'LOGOUT':
-            // AsyncStorage.setItem('token', null)
-            // AsyncStorage.setItem('user', null)  
+            AsyncStorage.setItem('token', null)
+            AsyncStorage.setItem('user', null)  
             // this.props.history.push(`/login`)
 
             return {
@@ -71,32 +78,13 @@ const rootReducer = (state, action) => {
                 email: action.email,
                 password: action.password
             }) 
-
-            // console.log('ASYNC STORAGE TOKEN', AsyncStorage.getItem('token') )
-            // fetch(`${API_URL}/auth`, {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     },
-            //     body: JSON.stringify({
-            //         email: action.email,
-            //         password: action.password
-            //     })
-            // })
-            
-            // .then(res => res.json() )
-
-            // .then( user => {
-            //     store.dispatch({ type: 'SAVE_USER', user })
-            //     console.log('TOKEN', user.token)
-            //     AsyncStorage.setItem('token', user.token)
-
-            //     // AsyncStorage.setItem('user', JSON.stringify(user))  
-            //     // history.push(`/match`)
-            // })
-                
-            //     .then( () => console.log('ASYNC STORAGE TOKEN', AsyncStorage.getItem('token') ))
-                     
+            .then( user => {
+                store.dispatch({ type: 'SAVE_USER', user })
+                AsyncStorage.setItem('token', user.token)
+                AsyncStorage.setItem('user', JSON.stringify(user))  
+                // history.push(`/match`)
+            })
+    
         case 'GET_POTENTIALS': 
             server.get(`${API_URL}/get_potential_matchees`)
             .then( potentials => {
@@ -116,19 +104,7 @@ const rootReducer = (state, action) => {
                 matchee_id: action.matchee_id
                 // current_user: action.current_user
             })
-            // server.post(`${API_URL}/matches/check`, {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //         'Authorization': state.user.token
-            //     },
-            //     body: JSON.stringify({
-            //         current_user_response: action.current_user_response,
-            //         matchee_id: action.matchee_id
-            //         // current_user: action.current_user
-            //     })
-            // })
-
+            
             // .then(res => res.json() )
             // .then(() => console.log(' THIS IS FROM ADD_RESPONSE'))
         } 
