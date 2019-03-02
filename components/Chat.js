f6c42024fd68e429950dec343daa224222d00963
claire <github email address>
2019-03-01 20:27:24 -0600

    checkpoint for the night, attempted fetching successful_matches in component did mount

--- components/Chat.js
+++ components/Chat.js
@@ -1,27 +1,107 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+// import React, { Component } from 'react'
+// import { Text, View } from 'react-native'
 import { connect } from 'react-redux'
+import { server } from '../server'
+
+
+// class _Chat extends Component {
+
+
+//   componentDidMount () {
+//     this.props.getMatches()
+//   }
+
+//   render() {
+//     return (
+//       <View>
+//         {console.log('CHAT COMPONENT, SUCCESSFUL MATCHES', this.props.successfulMatches)
+
+//         // .map(match => 
+//         //     <Text>NAME: {match.name}</Text>)
+//         } 
+//       </View>
+//     )
+//   }
+// }
+
+
+
+
+
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native'
+import { Container, Header, Content, List, ListItem, Thumbnail, Left, Body, Right, Button } from 'native-base';
 
 
 class _Chat extends Component {
 
-
-  componentDidMount () {
-    this.props.getMatches()
+  state = {
+    matches: []
   }
+  componentDidMount () {
+        // this.props.getMatches()
+
+        fetch('http://localhost:3000/successful_matches', {
+        
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${this.props.user.token}` 
+          }
+      })
+      .then(resp => resp.json())
+      .then(console.log)
+        .catch(error => console.log('ERRRRORS', error))
+        // .then(matches => this.setState({matches}))
+      }
 
   render() {
-    return (
-      <View>
-        {console.log('CHAT COMPONENT, SUCCESSFUL MATCHES', this.props.successfulMatches)
 
-        // .map(match => 
-        //     <Text>NAME: {match.name}</Text>)
-        } 
-      </View>
-    )
+    // let matches = (this.props.successfulMatches)
+    // let mapped = matches.map(match => console.log(match))x
+    // console.log(matches)
+    // console.log(typeof(matches))
+    // console.log(matches.keys())
+
+    // console.log(this.state.matches)
+
+
+    return (
+      <>
+      <Text>SOMETHING</Text>
+      </>
+    );
   }
 }
+
+const defaultImage = 'https://i.pinimg.com/originals/9f/81/2d/9f812d4cf313e887ef99d8722229eee1.jpg'
+
+const CardThing = () => {
+  <Container style={{marginTop: 100}}>
+        <Header />
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: {defaultImage} }} />
+              </Left>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note numberOfLines={5}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+}
+
+
+
 
 
 
@@ -29,7 +109,6 @@ const mapStateToProps = state => {
 
     return {
       user: state.user,
-      potentials: state.potentials,
       successfulMatches: state.successfulMatches
     }
 }
