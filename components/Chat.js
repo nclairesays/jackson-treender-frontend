6c7266a18f375a6b37438c495fb1784fd2903d57
claire <github email address>
2019-03-03 23:55:06 -0600

    logged results of props in chat

--- components/Chat.js
+++ components/Chat.js
@@ -11,13 +11,50 @@ class _Chat extends Component {
   }
 
   render() {
+    let matches = this.props.successfulMatches
+    console.log('SUCESSFUL MATCHES', matches) //returns Array of Objects (capitalized Array &Object) see below in the return for example
+    console.log('TYPEOF(MATCHES)', typeof(matches)) //returns object (lowercase object)
+
+
+   
+    // let names = matches.map( user => (user.name))   // returns TypeError: Cannot read property 'map' of null
+    // console.log('NAMES', names) //does not return anything because error above
+    // console.log('TYPEOF(NAMES)', typeof(names)) //does not return anything because error above
+
+
     return (
       <View>
-        {console.log('CHAT COMPONENT, SUCCESSFUL MATCHES', this.props.successfulMatches)
+        {/* {console.log('PROPS', this.props.successfulMatches) } */}
 
-        // .map(match => 
-        //     <Text>NAME: {match.name}</Text>)
-        } 
+        {/* {
+          LOGS THIS: 
+            PROPS Array [
+              Object {
+                "birthday": null,
+                "created_at": "2019-02-27T20:53:38.394Z",
+                "email": "ruth@flatiron.com",
+                "gender": "female",
+                "id": 6,
+                "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSacQBMGvS6Js7ja912qKfvit47jh-NziVq6NtvcB4rlA0KR4ko",
+                "name": "Ruth",
+                "password_digest": "$2a$10$WPVxp4Av/lhANkEHmsPvz.4Zl9jgVjUxaT1il2DgYqld3dFu7dcBe",
+                "updated_at": "2019-02-27T20:53:38.394Z",
+              },
+            ]
+        } */}
+
+
+        {/* {if i include the following console.log where i start mapping through it, then the console.log('PROPS',..) above will return null} */}
+        {/* {console.log("TESTING THIS", this.props.succhessfulMatches.map( m => m))} */}
+        {/* {
+          LOGS:
+          TypeError: Cannot read property 'map' of undefined
+        } */}
+
+
+
+
+
       </View>
     )
   }
